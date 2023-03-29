d2ff0046c38507d098a0f83dc9208970b23eb743
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-03-29 20:27:02 +0200

    Adding the tic-tac-toe project

---
+++ tic-tac-toe/main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    url_for('static', filename='style.css')
+    return render_template('index.html')
