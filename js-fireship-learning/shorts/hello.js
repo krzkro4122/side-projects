e62b69c58cc78f07cf6eaf517421f085c22b9f41
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-04-15 09:50:09 +0200

    Prime siv in rust and python with execution time measurment

---
+++ js-fireship-learning/shorts/hello.js
@@ -0,0 +1,9 @@
+const hello = () => {
+  console.log("Hi mom!");
+};
+
+const howAreYou = () => {
+  console.log("How are you?");
+};
+
+export { hello, howAreYou };
--- prime-siv/src/main.rs
+++ prime-siv/src/main.rs
@@ -1,8 +1,33 @@
-fn prime_siv() {
-    println!("I'am prime, the siv!")
+use std::time::Instant;
+
+fn prime_siv(n: u32) -> Vec<u32> {
+    let mut numbers = Vec::from_iter(2..n);
+    numbers.reverse();
+    let mut primes = Vec::new();
+
+    while numbers.len() > 0 {
+        let num = numbers.pop().unwrap();
+        primes.push(num);
+        numbers = numbers.iter().filter(|&n| n % num != 0).cloned().collect();
+    }
+
+    return primes;
+}
+
+fn parse_n() -> u32 {
+    let args: Vec<String> = std::env::args().collect();
+    let n = args[1]
+        .trim_end()
+        .parse::<u32>()
+        .expect("Wrong number format!");
+    return n;
 }
 
 fn main() {
-    println!("Hello, world!");
-    prime_siv()
+    let n = parse_n();
+    let start = Instant::now();
+    prime_siv(n);
+    // let result = prime_siv(n);
+    println!("Prime siv up to {} took: {:?}s", n, start.elapsed());
+    // println!("Result: {:?}", result);
 }
