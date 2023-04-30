9171d0f598d030279aad59db753964182aba4c1f
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-04-30 19:03:10 +0200

    cleanup of the string interpeter functions

--- the-pragmatic-progammer/19./generic_interpreter.py
+++ the-pragmatic-progammer/19./generic_interpreter.py
@@ -14,26 +14,27 @@ TRANSITIONS = {
     }
 }
 
-# NON-GENERIC
-string = r'"Lol",lele "Lawl\"\" "    \\'
-state = 'look_for_string'
-result = []
-part = ""
-for char in string:
-    state, action = TRANSITIONS[state].get(char) or TRANSITIONS[state]['default']
-    match action:
-        case 'ignore':
-            ...
-        case 'start_new_string':
-            part = ""
-        case 'add_current_to_string':
-            part += char
-        case 'finish_current_string':
-            result.append(part)
 
-print(result)
+def interpret_non_generic(string, initial_state):
+    # NON-GENERIC
+    result = []
+    part = ""
+    state = initial_state
+    for char in string:
+        state, action = TRANSITIONS[state].get(char) or TRANSITIONS[state]['default']
+        match action:
+            case 'ignore':
+                ...
+            case 'start_new_string':
+                part = ""
+            case 'add_current_to_string':
+                part += char
+            case 'finish_current_string':
+                result.append(part)
 
 # GENERIC
+
+
 class FSM:
     def __init__(self, transitions, initial_state) -> None:
         self.transitions = transitions
@@ -43,19 +44,33 @@ class FSM:
         self.state, action = self.transitions[self.state].get(event) or self.transitions[self.state]['default']
         return action
 
-fsm = FSM(TRANSITIONS, 'look_for_string')
-result = []
-part = ""
-for char in string:
-    action = fsm.accept(char)
-    match action:
-        case 'ignore':
-            ...
-        case 'start_new_string':
-            part = ""
-        case 'add_current_to_string':
-            part += char
-        case 'finish_current_string':
-            result.append(part)
 
-print(result)
+def interpret_generic(string, initial_state):
+    fsm = FSM(TRANSITIONS, initial_state)
+    result = []
+    part = ""
+    for char in string:
+        action = fsm.accept(char)
+        match action:
+            case 'ignore':
+                ...
+            case 'start_new_string':
+                part = ""
+            case 'add_current_to_string':
+                part += char
+            case 'finish_current_string':
+                result.append(part)
+    return result
+
+
+def main():
+    string = r'"Lol",lele "Lawl\"\" "    \\'
+    state = 'look_for_string'
+    result = interpret_non_generic(string, state)
+    print(result)
+    result = interpret_generic(string, state)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
