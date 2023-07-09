b193d5cb4e3c16857e8eb7ccab7875ac30e1dc6d
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-07-09 14:07:37 +0200

    WIP: working on auth, need to integrate JWTwq

--- golang/gopher-project/route/route.go
+++ golang/gopher-project/route/route.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 
 	"github.com/krzkro4122/echogogorm/controller"
-	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4"
 )
 
 func define_endpoints(e *echo.Echo) {
@@ -13,22 +13,26 @@ func define_endpoints(e *echo.Echo) {
 	e.Static("/assets", "./public/assets")
 	e.GET("/", controller.Index)
 
+	// Authentication
+	e.POST("/login", controller.Login)
+	e.POST("/register", controller.Register)
+
 	// Product
-	e.GET("/product/:id", controller.ReadProduct)
-	e.GET("/product", controller.ReadAllProducts)
-	e.PUT("/product/:id", controller.UpdateProduct)
-	e.POST("/product", controller.CreateProduct)
-	e.DELETE("/product/:id", controller.DeleteProduct)
+	// e.GET("/product/:id", controller.ReadProduct)
+	// e.GET("/product", controller.ReadAllProducts)
+	// e.PUT("/product/:id", controller.UpdateProduct)
+	// e.POST("/product", controller.CreateProduct)
+	// e.DELETE("/product/:id", controller.DeleteProduct)
 
 	// Category
-	e.GET("/category/:id", controller.ReadCategory)
-	e.GET("/category", controller.ReadAllCategories)
-	e.PUT("/category/:id", controller.UpdateCategory)
-	e.POST("/category", controller.CreateCategory)
-	e.DELETE("/category/:id", controller.DeleteCategory)
+	// e.GET("/category/:id", controller.ReadCategory)
+	// e.GET("/category", controller.ReadAllCategories)
+	// e.PUT("/category/:id", controller.UpdateCategory)
+	// e.POST("/category", controller.CreateCategory)
+	// e.DELETE("/category/:id", controller.DeleteCategory)
 
 	// Purchase
-	e.POST("/cart/buy", controller.BuyCart)
+	// e.POST("/cart/buy", controller.BuyCart)
 }
 
 func Serve(address string) {
