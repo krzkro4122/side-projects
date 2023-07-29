4ebe498e31512aa63b246831124daaee363a951f
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-07-29 15:45:24 +0200

    portfolio changes: - new version (v3) - renamed uj to university - renamed euroloop to company - deleted all unnecessary assets - tweaked the card styling - played arount with pointer-events and the cursor property

---
+++ hosted-apps/portfolio/v3/src/Card.jsx
@@ -0,0 +1,24 @@
+import "./Card.css";
+
+function Card({ title, subtitle, href, stack }) {
+  return (
+    <a href={href} target="_blank">
+      <div className="card">
+        <div className="card-content">
+          <main>
+            <h3 className="card-title">{title}</h3>
+            <h4 className="card-subtitle">{subtitle}</h4>
+          </main>
+          {stack &&
+            stack.length === 2 ?
+            <h4 className="stack">{stack[0]}<br/>{stack[1]}</h4>
+            :
+            <h4 className="stack">{stack[0]}<br/>{stack[1]}<br/>{stack[2]}<br/>{stack[3]}</h4>
+          }
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default Card;
