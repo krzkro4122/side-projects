077775d75b7ac95de1b18e6c893ac98c0a6f0226
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-07-30 16:33:31 +0200

    Changed the portfolio a whole lot, real happy about the results

--- hosted-apps/portfolio/v3/src/App.jsx
+++ hosted-apps/portfolio/v3/src/App.jsx
@@ -1,4 +1,6 @@
 import profilePic from "./assets/profile_pic.png";
+import linkedin from "./assets/linkedin.png";
+import github from "./assets/github.png";
 import SlidingText from "./SlidingText";
 import Card from "./Card";
 import HalfCard from "./HalfCard";
@@ -13,15 +15,15 @@ function App() {
           <img src={profilePic} id="profile" alt="My profile photo." />
         </div>
         <div id="cards">
-          <div className="halfcards">
+          <div id="halfcards">
             <HalfCard
-              title={"GitHub"}
-              subtitle={"My profile on GitHub"}
+              src={github}
+              alt={"My GitHub profile"}
               href={"https://github.com/krzkro4122"}
             />
             <HalfCard
-              title={"LinkedIn"}
-              subtitle={"My profile on LinkedIn"}
+              src={linkedin}
+              alt={"My LinkedIn profile"}
               href={"https://linkedin.com/in/krzysztof-krol1"}
             />
           </div>
@@ -55,7 +57,7 @@ function App() {
               'A service-oriented application that communicates with OpenAI to provide a great chatBot experience.'
             }
             href={"http://67.207.78.202:5173/"}
-            stack={["Frontend: React.js", "Backend: FastApi", "Service: Flask", "Db: PostgreSQL"]}
+            stack={["Frontend: React.js", "Backend: FastApi", "Service: Flask", "Database: PostgreSQL"]}
           />
           <Card
             title={"Cursum"}
--- hosted-apps/portfolio/v3/src/Card.jsx
+++ hosted-apps/portfolio/v3/src/Card.jsx
@@ -9,12 +9,13 @@ function Card({ title, subtitle, href, stack }) {
             <h3 className="card-title">{title}</h3>
             <h4 className="card-subtitle">{subtitle}</h4>
           </main>
-          {stack &&
-            stack.length === 2 ?
-            <h4 className="stack">{stack[0]}<br/>{stack[1]}</h4>
-            :
-            <h4 className="stack">{stack[0]}<br/>{stack[1]}<br/>{stack[2]}<br/>{stack[3]}</h4>
-          }
+          <div className="stack">
+            <h4>{stack[0]}<br/>{stack[1]}</h4>
+            {stack && stack.length === 4 &&
+              <h4 className="second-stack">{stack[2]}<br/>{stack[3]}</h4>
+            }
+          </div>
+
         </div>
       </div>
     </a>
