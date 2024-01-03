d57952a6282645ceda190b6e68661a533a639d3f
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-01-03 23:55:41 +0100

    manao work

--- python/manao/utils/todo.py
+++ python/manao/utils/todo.py
@@ -3,6 +3,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.todo import Todo
 
+
+async def get_all_todos(db_session):
+    return await Todo.find_all(db_session)
+
+
 async def change_content(todo_id: str, content: str, db_session: AsyncSession):
 	todo = await Todo.find_by_id(
 		db_session=db_session,
