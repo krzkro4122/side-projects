c23f8e9aa4cc5cfbb5f8d55e1ebadf3ce468c5e9
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-06-02 23:54:28 +0200

    I think data stuff done?

---
+++ python/eleks/backend/dataSwipe/src/dataloader.py
@@ -0,0 +1,25 @@
+import os
+from torch.utils.data import DataLoader
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+
+
+def get_dataloader():
+    return DataLoader(get_dataset(), batch_size=4, num_workers=1)
+
+
+def get_dataset(image_size=32):
+    transform = transforms.Compose(
+        [
+            transforms.Resize(image_size),
+            transforms.CenterCrop(image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(0.5, 0.5),
+        ]
+    )
+
+    return dset.CIFAR10(
+        root=os.path.abspath("../data"),
+        download=True,
+        # transform=transform,
+    )
