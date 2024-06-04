117d9cb395a5c8b2f75a9040f6172d3da00a20d3
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-06-04 23:21:34 +0200

    working stub!!!

--- python/eleks/backend/dataSwipe/views.py
+++ python/eleks/backend/dataSwipe/views.py
@@ -1,25 +1,49 @@
-from django.http import HttpResponse
+import json
+from django.http import HttpRequest, HttpResponse
 from django.template import loader
 
 from dataSwipe.src.dataloader import get_dataloader
 from dataSwipe.src.image_converter import ImageUtils
 
+BATCH_INDEX = "batch_index"
+OFFSET = "offset"
 
-def index(request):
+type ImageData = list[dict[str, any]]
+
+
+def index(request: HttpRequest):
+
     dataloader = get_dataloader()
     data_iterator = iter(dataloader)
-    image_tensors, labels = data_iterator.next()
 
-    image_base64s = [
-        ImageUtils.tensor2base64(image_tensor) for image_tensor in image_tensors
+    image_tensors, _labels = next(data_iterator)
+    batch_index = 0
+    offset = 0
+
+    if request.method == "POST":
+        last_batch_index = int(request.POST[BATCH_INDEX])
+        for i in range(last_batch_index + 2):
+            image_tensors, _labels = next(data_iterator)
+            batch_index = i
+
+        if offset:
+            image_tensors[offset], _labels = next(data_iterator)
+
+    images = [
+        {
+            "image_base64": ImageUtils.tensor2base64(image_tensor),
+            "meta": json.dumps({BATCH_INDEX: batch_index, OFFSET: i}),
+        }
+        for i, image_tensor in enumerate(image_tensors)
     ]
 
-    template = loader.get_template("base.html")
     return HttpResponse(
-        template.render(
+        loader
+        .get_template("images.html" if request.method == "GET" else "images_bare.html")
+        .render(
             request=request,
             context={
-                "images": image_base64s,
+                "images": images,
             },
         )
     )
