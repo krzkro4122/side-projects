69078d0cdf9cc1492a994762829bede6dc948789
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-06-06 02:56:53 +0200

    Working UI !!!!!!!

--- python/eleks/backend/dataSwipe/views.py
+++ python/eleks/backend/dataSwipe/views.py
@@ -1,7 +1,9 @@
+import json
 from django.http import Http404, HttpRequest, HttpResponse
 from django.template import loader
 
 from dataSwipe.src.views_utils import (
+    create_meta,
     load_images,
     create_image,
     create_images,
@@ -39,16 +41,29 @@ def smash_image_batch(request: HttpRequest):
     if request.method != "POST":
         raise Http404("Method not allowed")
 
-    batch_size = int(request.POST[Keys.BATCH_SIZE])
-    last_batch_index = int(request.POST[Keys.BATCH_INDEX])
+    request_batch_size = int(request.POST[Keys.BATCH_SIZE])
+    request_image_metas = [
+        request.POST[f"#hiddenInput{i}"] for i in range(request_batch_size)
+    ]
+
+    image_metas = [
+        json.loads(image_meta) for image_meta in request_image_metas
+    ]
+
+    maximum_batch_index = max([meta[Keys.BATCH_INDEX] for meta in image_metas])
 
     batch_index = 0
-    image_tensors, data_iterator = load_images(batch_size=batch_size)
+    _image_tensors, data_iterator = load_images(batch_size=request_batch_size)
+    image_tensors = []
 
-    for i in range(last_batch_index + 2):
-        image_tensors, _ = next(data_iterator)
+    for i in range(1, maximum_batch_index + 2):
+        _image_tensors, _ = next(data_iterator)
+
+        image_tensors_matching_index = [tensor for i, tensor in enumerate(_image_tensors) if image_metas[i][Keys.BATCH_INDEX] == batch_index]
+        if image_tensors_matching_index:
+            image_tensors.extend(image_tensors_matching_index)
+
         batch_index = i
-
 
     return HttpResponse(
         loader
@@ -56,11 +71,21 @@ def smash_image_batch(request: HttpRequest):
         .render(
             request=request,
             context={
-                Keys.IMAGES: create_images(
-                    batch_index=batch_index,
-                    batch_size=batch_size,
-                    image_tensors=image_tensors,
-                ),
+                Keys.IMAGES: {
+                    Keys.ENTRIES: [
+                        create_image(
+                            batch_index=meta[Keys.BATCH_INDEX] + 1,
+                            batch_size=request_batch_size,
+                            image_tensor=image_tensors[i],
+                            offset=meta[Keys.OFFSET],
+                        )
+                        for i, meta in enumerate(image_metas)
+                    ],
+                    Keys.META: create_meta(
+                        batch_size=request_batch_size,
+                        batch_index=batch_index,
+                    )
+                },
             },
         )
     )
