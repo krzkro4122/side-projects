b7cb92903478c09da6dafd055f18243244ae4172
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-06-06 02:21:48 +0200

    getting the last values each image on new reqs

--- python/eleks/backend/dataSwipe/views.py
+++ python/eleks/backend/dataSwipe/views.py
@@ -1,63 +1,13 @@
-import json
 from django.http import Http404, HttpRequest, HttpResponse
 from django.template import loader
 
-from dataSwipe.src.dataloader import get_dataloader
-from dataSwipe.src.image_converter import ImageUtils
+from dataSwipe.src.views_utils import (
+    load_images,
+    create_image,
+    create_images,
+    Keys,
 
-IMAGE_TENSORS = "image_tensors"
-BATCH_INDEX = "batch_index"
-BATCH_SIZE = "batch_size"
-OFFSET = "offset"
-IMAGES = "images"
-ENTRIES = "entries"
-META = "meta"
-
-type ImageData = list[dict[str, any]]
-
-
-def _create_meta(batch_index: int, batch_size: int, i=0):
-    return json.dumps({
-        BATCH_INDEX: batch_index,
-        OFFSET: i,
-        "batch_size": batch_size,
-    })
-
-
-def _create_image(
-    batch_index: int,
-    batch_size: int,
-    image_tensor,
-    i: int
-):
-    return {
-        "image_base64": ImageUtils.tensor2base64(image_tensor),
-        META: _create_meta(batch_size=batch_size, batch_index=batch_index, i=i),
-        "index": batch_index * batch_size + i,
-    }
-
-
-def _create_images(**kwargs):
-    image_tensors = kwargs[IMAGE_TENSORS]
-    del kwargs[IMAGE_TENSORS]
-    return {
-        ENTRIES: [
-            _create_image(**{
-                **kwargs,
-                "i": i,
-                "image_tensor": image_tensor
-            })
-            for i, image_tensor in enumerate(image_tensors)
-        ],
-        META: _create_meta(**kwargs)
-    }
-
-
-def _load_images(batch_size: int):
-    dataloader = get_dataloader(batch_size=batch_size)
-    data_iterator = iter(dataloader)
-    image_tensors, _ = next(data_iterator)
-    return image_tensors, data_iterator
+)
 
 
 def index(request: HttpRequest):
@@ -67,7 +17,7 @@ def index(request: HttpRequest):
     batch_size = 5 # TODO - Get from UI in the request
     batch_index = 0 # TODO - Get from a DB or local storage
 
-    image_tensors, _ = _load_images(batch_size=batch_size)
+    image_tensors, _ = load_images(batch_size=batch_size)
 
     return HttpResponse(
         loader
@@ -75,7 +25,7 @@ def index(request: HttpRequest):
         .render(
             request=request,
             context={
-                IMAGES: _create_images(
+                Keys.IMAGES: create_images(
                     batch_index=batch_index,
                     batch_size=batch_size,
                     image_tensors=image_tensors,
@@ -89,23 +39,24 @@ def smash_image_batch(request: HttpRequest):
     if request.method != "POST":
         raise Http404("Method not allowed")
 
-    batch_index = 0
-    print(request.POST)
-    batch_size = int(request.POST[BATCH_SIZE])
-    image_tensors, data_iterator = _load_images(batch_size=batch_size)
+    batch_size = int(request.POST[Keys.BATCH_SIZE])
+    last_batch_index = int(request.POST[Keys.BATCH_INDEX])
 
-    last_batch_index = int(request.POST[BATCH_INDEX])
+    batch_index = 0
+    image_tensors, data_iterator = load_images(batch_size=batch_size)
+
     for i in range(last_batch_index + 2):
         image_tensors, _ = next(data_iterator)
         batch_index = i
 
+
     return HttpResponse(
         loader
-        .get_template("image_form.html")
+        .get_template("images_form.html")
         .render(
             request=request,
             context={
-                IMAGES: _create_images(
+                Keys.IMAGES: create_images(
                     batch_index=batch_index,
                     batch_size=batch_size,
                     image_tensors=image_tensors,
@@ -119,25 +70,27 @@ def pass_image(request: HttpRequest):
     if request.method != "POST":
         raise Http404("Method not allowed")
 
-    batch_index = 0
-    image_tensors, data_iterator = _load_images(batch_size=1)
+    request_batch_size = int(request.POST[Keys.BATCH_SIZE])
+    request_batch_index = int(request.POST[Keys.BATCH_INDEX])
+    request_offset = int(request.POST[Keys.OFFSET])
 
-    request_batch_index = int(request.POST[BATCH_INDEX])
-    request_batch_size = int(request.POST[BATCH_SIZE])
-    for i in range(request_batch_index + 2):
+    image_tensors, data_iterator = load_images(batch_size=request_batch_size)
+
+    for _ in range(request_batch_index + 2):
         image_tensors, _ = next(data_iterator)
-        batch_index = i
 
     return HttpResponse(
         loader
-        .get_template("image.html")
+        .get_template("image_form.html")
         .render(
             request=request,
             context={
-                IMAGES: _create_images(
-                    batch_index=batch_index,
-                    batch_size=request_batch_size,
-                    image_tensors=image_tensors,
+                Keys.IMAGE:
+                    create_image(
+                        batch_index=request_batch_index + 1,
+                        batch_size=request_batch_size,
+                        image_tensor=image_tensors[request_offset],
+                        offset=request_offset,
                 ),
             },
         )
--- python/eleks/backend/root/urls.py
+++ python/eleks/backend/root/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", dataSwipe.views.index),
     path("smash_image_batch", dataSwipe.views.smash_image_batch),
-    path("pass_image/", dataSwipe.views.pass_image),
+    path("pass_image", dataSwipe.views.pass_image),
     path("__reload__/", include("django_browser_reload.urls"))
 ]
