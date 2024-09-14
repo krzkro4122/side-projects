7910b33f4fe39be95a627536a641a2993da2c673
krzkro4122 <krolkrzysztof1999@gmail.com>
2024-09-14 13:57:22 +0200

    chaser start

---
+++ hosted-apps/chaser/app/components/Chaser.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function Chaser() {
+    return (
+        <div id="chaser-canvas" className="flex-grow w-full rounded-md border-2">
+        
+        </div>
+    );
+  }
